53538f2a558832e001af6aa448d26649c176a432
kkangh00n <kig2454@gmail.com>
2024-01-15 21:33:05 +0900

    text : 재고감소 로직 테스트

---
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -0,0 +1,42 @@
+package com.example.stock.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceTest {
+
+    @Autowired
+    private StockService stockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void before() {
+        stockRepository.save(new Stock(1L, 100L));
+    }
+
+    @AfterEach
+    public void after(){
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    public void 재고감소() {
+        stockService.decrease(1L, 1L);
+
+        Stock stock = stockRepository.findById(1L)
+            .orElseThrow();
+
+        Assertions.assertThat(stock.getQuantity()).isEqualTo(99);
+    }
+}
