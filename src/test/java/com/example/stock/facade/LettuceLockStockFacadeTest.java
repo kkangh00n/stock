3bd7fc44f30ed33d9b87b27ecdc6bc7c10c1b1f4
kkangh00n <kig2454@gmail.com>
2024-01-16 17:17:30 +0900

    feat : 해결책5 LettuceLock 락

---
+++ src/test/java/com/example/stock/facade/LettuceLockStockFacadeTest.java
@@ -0,0 +1,62 @@
+package com.example.stock.facade;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LettuceLockStockFacadeTest {
+
+    @Autowired
+    private LettuceLockStockFacade lettuceLockStockFacade;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    public void before() {
+        stockRepository.saveAndFlush(new Stock(1L, 100L));
+    }
+
+    @AfterEach
+    public void after() {
+        stockRepository.deleteAll();
+    }
+
+    @Test
+    public void 동시에_100개_요청() throws InterruptedException {
+        int threadCount = 100;
+
+        ExecutorService executorService = Executors.newFixedThreadPool(30);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    lettuceLockStockFacade.decrease(1L, 1L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    //latch 숫자를 줄여나감
+                    latch.countDown();
+                }
+            });
+        }
+
+        //latch 숫자가 모두 줄어들 때까지 기다림
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertThat(stock.getQuantity()).isEqualTo(0);
+    }
+}
